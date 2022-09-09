c37a09f748fefe8f971b54c2b6176256df1d8b29
Kyoichi Iwasaki <kyiwasak@microsoft.com>
2022-09-09 08:11:55 +0000

    Modified reduce, partition processes

--- src/partition.py
+++ src/partition.py
@@ -3,8 +3,33 @@
 
 import argparse
 import os
+from core.structure import *
+from core.partitioner import *
 
 parser = argparse.ArgumentParser("partition")
 
 parser.add_argument("--model_input_reduced", type=str, help="the reduced model input")
+parser.add_argument("--distance", type=str, help="the distance file")
 parser.add_argument("--model_input_list", type=str, help="the list of partitioned model input")
+
+args = parser.parse_args()
+print("Argument 1: %s" % args.model_input_reduced)
+print("Argument 2: %s" % args.distance)
+print("Argument 3: %s" % args.model_input_list)
+
+
+## Instanciation
+partitioner = ProblemPartitioner()
+model_input_reduced = ModelInput()
+model_input_reduced.initInputFromFile(args.model_input_reduced + "/order_reduced.csv", args.distance)
+
+## Partition process
+max_package_num = 30
+model_input_list = partitioner.partition(model_input_reduced, max_package_num)
+
+## Save the results
+i = 0
+for model_input_partition in model_input_list:
+    model_input_partition_file = os.path.join(args.model_input_list, f"order_partition_{i}.csv")
+    model_input_partition.toOrderDF().to_csv(model_input_partition_file, index=False)
+    i += 1
